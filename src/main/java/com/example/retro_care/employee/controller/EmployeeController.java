024a967afb9c2f434473b6fa0fd9d04108347bae
Tran Thanh Son <sonva2002@gmail.com>
2023-09-15 15:35:06 +0700

    [SONTT][employee]update model

--- src/main/java/com/example/retro_care/employee/controller/EmployeeController.java
+++ src/main/java/com/example/retro_care/employee/controller/EmployeeController.java
@@ -1,10 +1,16 @@
 package com.example.retro_care.employee.controller;
 
+import com.example.retro_care.employee.model.Employee;
 import com.example.retro_care.employee.service.IEmployeeService;
+import org.apache.coyote.Request;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
@@ -12,4 +18,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     @Autowired
     private IEmployeeService employeeService;
+    @GetMapping("/get_list/{page}/{limit}")
+    public ResponseEntity<Page<Employee>> getListEmployee(@PathVariable(value = "page", required = false) Integer page,
+                                                          @PathVariable(value = "limit", required = false) Integer limit) {
+        Pageable pageable = PageRequest.of(page, limit);
+      Page<Employee> employees=  employeeService.getListEmployee(pageable);
+        if (employees.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else{
+            return new ResponseEntity<>(employees, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/search-list/{page}/{limit}/{sort}")
+    public ResponseEntity<Page<Employee>> searchEmployee(@PathVariable(value = "page", required = false) Integer page,
+                                                         @PathVariable(value = "limit", required = false) Integer limit,
+                                                         @PathVariable(value = "sort", required = false) String sort,
+                                                         @RequestParam(value = "role", required = false) Long idRole,
+                                                         @RequestParam(value = "name", required = false) String nameEmployee) {
+        Pageable pageable = PageRequest.of(page, limit, Sort.by(sort));
+        if (idRole == null && nameEmployee.isEmpty()) {
+            return new ResponseEntity<>(employeeService.getListEmployee(pageable), HttpStatus.OK);
+        } else {
+            if (employeeService.searchEmployee(pageable, idRole, nameEmployee).isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            } else {
+                return new ResponseEntity<>(employeeService.searchEmployee(pageable, idRole, nameEmployee), HttpStatus.OK);
+            }
+        }
+    }
 }
--- src/main/java/com/example/retro_care/employee/service/IEmployeeService.java
+++ src/main/java/com/example/retro_care/employee/service/IEmployeeService.java
@@ -1,4 +1,47 @@
 package com.example.retro_care.employee.service;
 
+import com.example.retro_care.employee.model.Employee;
+import com.example.retro_care.user.model.AppRole;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface IEmployeeService {
+    /**
+     * Create: SonTT
+     * Date create: 15/09/2023
+     * Function: Create a method that takes data and returns it to the Page
+     * @param pageable
+     * @return Page
+     */
+    Page<Employee> getListEmployee(Pageable pageable);
+
+    /**
+     * Create: SonTT
+     * Date create: 15/09/2023
+     * Function: Create a method that takes data and returns it to the Page
+     * @param pageable
+     * @param id
+     * @param name
+     * @return Page
+     */
+    Page<Employee> searchEmployee(Pageable pageable, Long id , String name);
+
+    /**
+     * Create: SonTT
+     * Date create: 15/09/2023
+     * Function: Get data AppUser from database
+     * @return List
+     */
+    List<AppRole> getRole();
+
+    /**
+     * Create: SonTT
+     * Date create: 15/09/2023
+     * Function: Delete employee with id
+     * @param id
+     * @return boolean
+     */
+    boolean deleteEmployee(Long id);
 }
