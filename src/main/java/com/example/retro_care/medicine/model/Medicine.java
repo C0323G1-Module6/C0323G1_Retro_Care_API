3d4d816e9776c298cd9922585df29b8f2996d2dc
AnhDao19112001 <phantaanhdao@gmail.com>
2023-09-15 09:19:51 +0700

    [DaoPTA][medicine] Create model medicine

--- src/main/java/com/example/retro_care/medicine/model/Medicine.java
+++ src/main/java/com/example/retro_care/medicine/model/Medicine.java
@@ -1,4 +1,23 @@
 package com.example.retro_care.medicine.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Medicine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String code;
+    private String name;
+    private Double price;
+    private Long quantity;
+    private Float vat;
+    @Column(columnDefinition = "LONGTEXT")
+    private String note;
+    private String make;
+    private String origin;
+    private Float retail_profits;
+    @Column(columnDefinition = "TEXT")
+    private String active_element;
+    private Boolean flag_deleted;
 }
