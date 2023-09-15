e0b99b24726138434cbc74ac79fda9cf923bc139
mrhuyle <mrhuyle@gmail.com>
2023-09-15 16:26:30 +0700

    [HuyL][home] update controller basic - ongoing

--- src/main/java/com/example/retro_care/home/repository/HomeRepository.java
+++ src/main/java/com/example/retro_care/home/repository/HomeRepository.java
@@ -1,2 +1,9 @@
-package com.example.retro_care.home.repository;public class HomeRepository {
+package com.example.retro_care.home.repository;
+
+import com.example.retro_care.medicine.model.Medicine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface HomeRepository extends JpaRepository<Medicine,Long> {
+
 }
