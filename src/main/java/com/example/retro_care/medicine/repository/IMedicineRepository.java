d3c1fd86b841b694dc542613b73753e01a325b63
AnhDao19112001 <phantaanhdao@gmail.com>
2023-09-15 15:35:31 +0700

    [DaoPTA]

--- src/main/java/com/example/retro_care/medicine/repository/IMedicineRepository.java
+++ src/main/java/com/example/retro_care/medicine/repository/IMedicineRepository.java
@@ -1,4 +1,12 @@
 package com.example.retro_care.medicine.repository;
 
-public interface IMedicineRepository {
+import com.example.retro_care.medicine.model.Medicine;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IMedicineRepository extends JpaRepository<Medicine, Long> {
+
+    Page<Medicine> findAll(Pageable pageable);
+
 }
