ce12bdb3252a65de1016525a0fdfae289af321e0
NguyenVietCao <vietcao2510@gmail.com>
2023-09-15 11:54:42 +0700

    [CaoNV][kind of medicine]model

--- src/main/java/com/example/retro_care/kind_of_medicine/service/IKindOfMedicineService.java
+++ src/main/java/com/example/retro_care/kind_of_medicine/service/IKindOfMedicineService.java
@@ -1,4 +1,19 @@
 package com.example.retro_care.kind_of_medicine.service;
 
+import com.example.retro_care.kind_of_medicine.model.KindOfMedicine;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface IKindOfMedicineService {
+    List<KindOfMedicine>getListKindOfMedicine();
+    Page<KindOfMedicine>getPageKindOfMedicine(Pageable pageable,String searchCode,String searchName);
+     KindOfMedicine getKindOfMedicineById(Integer id);
+
+    void deleteKindOfMedicineById(Integer id);
+    Boolean deleteKindOfMedicine(int id);
+    void addKindOfMedicine(KindOfMedicine kindOfMedicine);
+    void editKindOfMedicine(KindOfMedicine kindOfMedicine);
+
 }
