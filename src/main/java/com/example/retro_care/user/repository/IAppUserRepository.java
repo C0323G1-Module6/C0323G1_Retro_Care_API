85033a1f17b27169cd5cad7a1c8dd301354eac53
hoangnhat3103@gmail.com <hoangnhat3103@gmail.com>
2023-09-15 14:46:10 +0700

    [NhatNHH][user] login account

--- src/main/java/com/example/retro_care/user/repository/IAppUserRepository.java
+++ src/main/java/com/example/retro_care/user/repository/IAppUserRepository.java
@@ -2,6 +2,8 @@ package com.example.retro_care.user.repository;
 
 import com.example.retro_care.user.model.AppUser;
 import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IAppUserRepository extends JpaAttributeConverter<AppUser,Long> {
+public interface IAppUserRepository extends JpaRepository<AppUser,Long> {
+    AppUser findAppUserByUserName(String userName);
 }
--- src/main/java/com/example/retro_care/user/service/IAppUserService.java
+++ src/main/java/com/example/retro_care/user/service/IAppUserService.java
@@ -1,6 +1,13 @@
 package com.example.retro_care.user.service;
 
+import com.example.retro_care.user.dto.AppUserDto;
+import com.example.retro_care.user.model.AppUser;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.validation.BindingResult;
+
+import java.util.Map;
 
 public interface IAppUserService extends UserDetailsService {
+    UserDetails loadUserByAppUserDto(AppUser appUser);
 }
