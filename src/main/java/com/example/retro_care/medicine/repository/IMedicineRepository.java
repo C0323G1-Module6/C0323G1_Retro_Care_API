aebbee64e1d5d1611688846d8341938f3ada8687
AnhDao19112001 <phantaanhdao@gmail.com>
2023-09-17 08:15:49 +0700

    [DaoPTA][Retro Care] update medicine

--- src/main/java/com/example/retro_care/medicine/repository/IMedicineRepository.java
+++ src/main/java/com/example/retro_care/medicine/repository/IMedicineRepository.java
@@ -92,6 +92,9 @@ public interface IMedicineRepository extends JpaRepository<Medicine, Long> {
             "    unit u ON ud.unit_id = u.id where m.flag_deleted = false", nativeQuery = true)
     Page<Medicine> findAll(Pageable pageable);
 
+    @Query(value = "update medicine set flag_deleted = true where medicine.id = :id",nativeQuery = true)
+    void deleteMedicineById(@Param("id") Long id);
+
     /**
      * author: VuNL
      * date create: 16/09/2023
