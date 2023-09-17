1fd73685b24e1992efa4a0aebc2c92fd44d44935
hoangnhat3103@gmail.com <hoangnhat3103@gmail.com>
2023-09-17 22:52:56 +0700

    [NhatNHH][User]update login with facebook

--- src/main/java/com/example/retro_care/user/controller/AppUserController.java
+++ src/main/java/com/example/retro_care/user/controller/AppUserController.java
@@ -1,17 +1,16 @@
 package com.example.retro_care.user.controller;
 
+import com.example.retro_care.user.common.RandomStringGenerator;
 import com.example.retro_care.user.common.ValidateAppUser;
 import com.example.retro_care.user.config.JwtTokenUtil;
 import com.example.retro_care.user.dto.AppUserDto;
-import com.example.retro_care.user.dto.FacebookLoginRequest;
+import com.example.retro_care.user.dto.FacebookMailRequest;
 import com.example.retro_care.user.model.AppUser;
 import com.example.retro_care.user.model.JwtResponse;
 import com.example.retro_care.user.service.IAppUserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -24,7 +23,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.Cookie;
 import javax.validation.Valid;
 import java.util.*;
 
@@ -32,12 +30,7 @@ import java.util.*;
 @CrossOrigin("*")
 @RequestMapping("/api/user")
 public class AppUserController {
-    /**
-     * Creater: NhatNHH
-     * Created date: 15-09-2023
-     * <p>
-     * Function: login, logout from account, google or facebook
-     */
+
     @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
@@ -48,9 +41,11 @@ public class AppUserController {
     private PasswordEncoder passwordEncoder;
 
     /**
-     * @param appUserDto:
-     * @param bindingResult
-     * @return if authortication then return jwt, roles,
+     * method loginByAccount
+     * Creater NhatNHH
+     * Date 15-09-2023
+     * param AppUserDto appUserDto, BindingResult bindingResult
+     * return JwtResponse(jwtToken)
      */
     @PostMapping("/login-by-username")
     public ResponseEntity<?> loginByAccount(@Valid @RequestBody AppUserDto appUserDto,
@@ -84,35 +79,58 @@ public class AppUserController {
         BeanUtils.copyProperties(appUserDto, appUser);
 
         UserDetails userDetails = appUserService.loadUserByUsername(appUser.getUserName());
+
         String jwtToken = jwtTokenUtil.generateToken(userDetails);
 
-        ResponseCookie cookie = ResponseCookie.from("JWT", jwtToken)
-                .httpOnly(false)
-                .secure(false)
-                .path("/")
-                .maxAge(60*60*60)
-                .build();
-
-
+//        ResponseCookie cookie = ResponseCookie.from("JWT", jwtToken)
+//                .httpOnly(false)
+//                .secure(false)
+//                .path("/")
+//                .maxAge(60*60*60)
+//                .build();
 
         return ResponseEntity
                 .ok()
-                .header(HttpHeaders.SET_COOKIE, cookie.toString())
-                .body(new JwtResponse(jwtToken, userDetails.getUsername(), userDetails.getAuthorities(), true));
+//                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(new JwtResponse(jwtToken));
 
     }
+    /**
+     * method loginByFacebook
+     * Creater NhatNHH
+     * Date 16-09-2023
+     * param FacebookMailRequest facebookMailRequest
+     * return JwtResponse(jwtToken)
+     */
+    @PostMapping("/login-by-facebook")
+    public ResponseEntity<?> loginByFacebook(@RequestBody FacebookMailRequest facebookMailRequest) {
+        String facebookMail = facebookMailRequest.getFacebookMail();
+        Boolean checkExistAppUser = appUserService.existsByUsername(facebookMail);
+        if(!checkExistAppUser){
+            AppUser appUser = new AppUser();
+            appUser.setUserName(facebookMail);
+            String randomPassword = RandomStringGenerator.generateRandomString();
+            System.out.println(randomPassword);
+            appUser.setPassword(passwordEncoder.encode(randomPassword));
+            appUserService.createNewAppUser(appUser);
+        }
+        UserDetails userDetails = appUserService.loadUserByUsername(facebookMail);
 
-    @GetMapping("/login-by-facebook")
-    public ResponseEntity<?> loginByFacebook(@RequestBody FacebookLoginRequest facebookLoginRequest) {
-        System.out.println(facebookLoginRequest.getAccessTokenFacebook());
+        String jwtToken = jwtTokenUtil.generateToken(userDetails);
+
         return ResponseEntity
                 .ok()
-                .body(facebookLoginRequest
-                        .getAccessTokenFacebook());
+                .body(new JwtResponse(jwtToken));
     }
-
+    /**
+     * method registerByCustomer
+     * Creater NhatNHH
+     * Date 15-09-2023
+     * param AppUserDto appUserDto, BindingResult bindingResult
+     * return ResponseEntity.ok(Register Success)
+     */
     @PostMapping("/register-by-customer")
-    public ResponseEntity<?> register(@Valid @RequestBody AppUserDto appUserDto,
+    public ResponseEntity<?> registerByCustomer(@Valid @RequestBody AppUserDto appUserDto,
                                       BindingResult bindingResult) {
         new AppUserDto().validate(appUserDto, bindingResult);
         ValidateAppUser.checkValidateConfirmAppUserPassword(appUserDto.getConfirmPassword(), bindingResult);
@@ -148,6 +166,13 @@ public class AppUserController {
         return ResponseEntity.ok("Đăng ký thành công, vui lòng bấm nút đăng nhập");
     }
 
+    /**
+     * method registerByManager
+     * Creater NhatNHH
+     * Date 15-09-2023
+     * param String userName
+     * return ResponseEntity.ok(Register Success)
+     */
     @PostMapping("/register-by-manager")
     public ResponseEntity<?> registerByManager(@RequestParam String userName) {
         //check validate userName
@@ -171,11 +196,27 @@ public class AppUserController {
         if (!checkAddNewAppUser) {
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Đăng ký thất bại, vui lòng chờ trong giây lất");
+                    .body("Đăng ký thất bại, vui lòng chờ trong giây lát");
         }
-        return ResponseEntity.ok("Đăng ký thành công, vui lòng đăng nhập");
+        return ResponseEntity.ok("Đăng ký thành công");
     }
-
+    /**
+     * method logout
+     * Creater NhatNHH
+     * Date 15-09-2023
+     * param String userName
+     * return ResponseEntity.ok(Logout Success)
+     */
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(@RequestParam String userName){
+        Boolean logout = appUserService.logout(userName);
+        if(logout){
+            return ResponseEntity.ok("Đăng xuất thành công");
+        }
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Đăng xuất thất bại, vui lòng chờ trong giây lát");
+    }
 
 
 }
