98fadc75116a2c90caea6ad9d35100889258fe92
cuong19962012 <tancuong.it@gmail.com>
2023-09-19 20:33:16 +0700

    [Invoice][CuongHLT]merge from 2 branch

--- src/main/java/com/example/retro_care/invoice/service/IInvoiceService.java
+++ src/main/java/com/example/retro_care/invoice/service/IInvoiceService.java
@@ -1,11 +1,10 @@
 package com.example.retro_care.invoice.service;
 
+import com.example.retro_care.invoice.model.IInvoiceResult;
 import com.example.retro_care.invoice.model.Invoice;
 import com.example.retro_care.invoice.model.InvoiceDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.List;
 
 public interface IInvoiceService {
     /**
@@ -40,19 +39,20 @@ public interface IInvoiceService {
      * Create by: HuyHD;
      * Date create: 15/09/2023
      * Function: displays a paginated list of invoice;
-     *
-     * @param : page (page number), limit(number of elements in the page);
      * @return : paginated invoice list with limit number of molecules per page.
      */
     Page<Invoice> findAllInvoice(Pageable pageable);
+    Page<IInvoiceResult> findAllInvoiceResult(Pageable pageable);
 
     void deleteInvoice(Long id);
 
     Invoice findById(Long id);
 
-    List<Invoice> searchInvoice(String start_date,
-                                String end_date,
-                                String start_time,
-                                String end_time,
-                                String sort_column);
+   Page<Invoice> searchInvoice(Pageable pageable,
+                                     String startDate,
+                                     String endDate,
+                                     String startTime,
+                                     String endTime,
+                                     String sortColumn
+                              );
 }
