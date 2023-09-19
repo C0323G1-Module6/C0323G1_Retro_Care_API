7393d118fe9dcf851bf5e765dba3ddc443d88675
AnhDao19112001 <phantaanhdao@gmail.com>
2023-09-19 23:17:50 +0700

    [DaoPTA][Search by medicine]

--- src/main/java/com/example/retro_care/medicine/service/IMedicineService.java
+++ src/main/java/com/example/retro_care/medicine/service/IMedicineService.java
@@ -37,7 +37,7 @@ public interface IMedicineService {
      * @param pageable pagination of medication list
      * @return : Medicine list with pagination
      */
-    Page<Medicine> findAll(Pageable pageable);
+    Page<Medicine> findAll(Pageable pageable, String search);
 
     /**
      * author: DaoPTA
@@ -51,49 +51,47 @@ public interface IMedicineService {
 
     List<Medicine> getAll();
 
-//    /**
-//     * author: DaoPTA
-//     * workday: 17/09/2023
-//     * Search for drugs by medicine code
-//     *
-//     * @param pageable Pagination after search
-//     * @param searchByCode Parameters used to search
-//     * @return approximate drug code with filter.
-//     */
-//    Page<Medicine> searchByCodeMedicine(Pageable pageable,String searchByCode);
-//
-//    /**
-//     * author: DaoPTA
-//     * workday: 17/06/2023
-//     * Search by medicine name
-//     *
-//     * @param pageable Pagination after search
-//     * @param searchByName Parameters used to search
-//     * @return the drug name that approximates the filter
-//     */
-//    Page<Medicine> searchByNameMedicine(Pageable pageable,String searchByName);
-//
-//    /**
-//     * author: DaoPTA
-//     * workday: 17/09/2023
-//     * Search by active element of medicine
-//     *
-//     * @param pageable Pagination after search
-//     * @param searchByActiveElement Parameters used to search
-//     * @return the drug's active ingredient approximated by the filter
-//     */
-//    Page<Medicine> searchActiveElement(Pageable pageable,String searchByActiveElement);
-//
-//    /**
-//     * author: DaoPTA
-//     * workday: 17/09/2023
-//     * Search by kind of medicine
-//     *
-//     * @param pageable Pagination after search
-//     * @param searchByNameKindOf Parameters used to search
-//     * @return the drug group of the drug approximated by the filter
-//     */
-//    Page<Medicine> searchByKind(Pageable pageable,String searchByNameKindOf);
-//
-    Page<Medicine> searchByMedicine(Pageable pageable, String searchByName, String searchByCode, String searchByActiveElement, String searchByNameKindOf);
+    /**
+     * author: DaoPTA
+     * workday: 17/09/2023
+     * Search for drugs by medicine code
+     *
+     * @param pageable Pagination after search
+     * @param searchByCode Parameters used to search
+     * @return approximate drug code with filter.
+     */
+    Page<Medicine> searchByCodeMedicine(Pageable pageable,String searchByCode);
+
+    /**
+     * author: DaoPTA
+     * workday: 17/06/2023
+     * Search by medicine name
+     *
+     * @param pageable Pagination after search
+     * @param searchByName Parameters used to search
+     * @return the drug name that approximates the filter
+     */
+    Page<Medicine> searchByNameMedicine(Pageable pageable,String searchByName);
+
+    /**
+     * author: DaoPTA
+     * workday: 17/09/2023
+     * Search by active element of medicine
+     *
+     * @param pageable Pagination after search
+     * @param searchByActiveElement Parameters used to search
+     * @return the drug's active ingredient approximated by the filter
+     */
+    Page<Medicine> searchActiveElement(Pageable pageable,String searchByActiveElement);
+
+    /**
+     * author: DaoPTA
+     * workday: 17/09/2023
+     * Search by kind of medicine
+     *
+     * @param pageable Pagination after search
+     * @param searchByNameKindOfMedicine Parameters used to search
+     * @return the drug group of the drug approximated by the filter
+     */
+    Page<Medicine> searchByNameKindOfMedicine(Pageable pageable,String searchByNameKindOfMedicine);
 }
